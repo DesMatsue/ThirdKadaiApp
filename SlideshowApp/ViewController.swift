8e45a5b31ec16dd5785fa7a431014b5309102e04
DesMatsue <takakura-y@macd1189.local>
2017-07-20 00:40:44 -0700

    new

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    /*
+     MemberVariables
+     */
+    @IBOutlet weak var imagePanel: UIImageView!
+    var resume:Bool=false
+    var timer:Timer?
+    var index:Int=0
+    // 画像リスト
+    // 作業対象
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +28,31 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // インデックスを増分しながら表示
+    func slideShow(){
+        // 作業対象
+        // インデックスをリスト数まで増分
+        // 作業対象
+    }
+    /*
+     ButtonActions
+     */
+    @IBAction func resume(_ sender: Any) {
+        if let timer = self.timer{
+            self.timer=timer
+            self.resume = !resume
+        }else{
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target:self,selector: #selector(slideShow),userInfo:nil, repeats: true)
+            // 画像リストを取得
+            // 作業対象
+        }
+    }
+    @IBAction func `return`(_ sender: Any) {
+        self.index -= 1
+    }
+    @IBAction func proceed(_ sender: Any) {
+        self.index += 1
+    }
 }
 
